2ef0dc9ddae2196b1592bc6313953ab6c0c47431
rishabhjain7b <rishabhjain7b@gmail.com>
2018-04-02 12:23:18 +0530

    modified fifth

--- sq_3bit/sq_3bit.v
+++ sq_3bit/sq_3bit.v
@@ -4,13 +4,22 @@ module sq_3bit
 (input [2:0]in,
  output [5:0]out);
 
-wire [2:0]w;
+/*wire [2:0]w;
 
 assign out[0]=in[0],
 	{w[0],out[1]}=(in[1]&in[0])<<1,
 	{w[1],out[2]}=((in[2]&in[0])<<1)+in[1]+w[0],
 	{w[2],out[3]}=((in[2]&in[1])<<1)+w[1],
 	{out[5],out[4]}=in[2]+w[2];
+*/
+
+wire [5:0]w1,w2,w3;
+
+assign w1=in[0]?in:0,
+	w2=in[1]?in<<1:0,
+	w3=in[2]?in<<2:0,
+	out=w1+w2+w3;
+
 endmodule
 
 module sq_3bit_tb;
