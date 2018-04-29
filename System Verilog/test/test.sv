e5eb1aeed1638c44950746cec37093c43752d8c5
rishabhjain7b <rishabhjain7b@gmail.com>
2018-04-30 00:01:24 +0530

    eleventh

---
+++ System Verilog/test/test.sv
@@ -0,0 +1,17 @@
+// Code for testing Literal value assignments
+
+module test #(parameter size=10);
+  logic [size-1:0]a,b,c,d,e;
+  
+  initial
+  begin
+    a='b0;
+    b='bx;
+    c='bz;
+    d='b1;
+    e='1;
+  end
+  
+  initial 
+  $monitor("a=%b | b=%b | c=%b | d=%b | e=%b ",a,b,c,d,e);  
+endmodule
