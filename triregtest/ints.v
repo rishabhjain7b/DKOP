2f618f251a0fff924112436e80ddee8fb595516c
rishabhjain7b <rishabhjain7b@gmail.com>
2018-05-01 12:28:13 +0530

    eleventh

--- triregtest/ints.v
+++ triregtest/ints.v
@@ -2,7 +2,8 @@
 
 module ints;
 
-reg[31:0] IntA, IntB, IntC, IntD;
+real IntA, IntB, IntC, IntD;
+reg[3:0]s;
 initial 
 begin: b1
 
@@ -13,6 +14,8 @@ IntC = -'sd 12 / 3; // The result is -4.
 IntD = -4'sd 12 / 3; // -4'sd12 is the negative of the 4-bit
 // quantity 1100, which is -4. -(-4) = 4.
 // The result is 1.
+s=-4'd12;
+
 $displayh(IntA);
 $display(IntB);
 $displayh(IntC);
