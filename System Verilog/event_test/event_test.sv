d413817f0e221efc77018cbc1df98e08c6728be2
rishabhjain7b <rishabhjain7b@gmail.com>
2018-05-01 15:01:00 +0530

    eleventh

---
+++ System Verilog/event_test/event_test.sv
@@ -0,0 +1,21 @@
+// Code for testing events in SV
+
+module event_test;
+event e1,e2;
+
+initial
+begin
+	$display("@%0d: 1: before trigger", $time);
+	-> e1;
+	@e2;
+	$display("@%0d: 1: after trigger", $time);
+end
+
+initial
+begin
+	$display("@%0d: 2: before trigger", $time);
+	-> e2;
+	@e1;
+	$display("@%0d: 2: after trigger", $time);
+end
+endmodule
