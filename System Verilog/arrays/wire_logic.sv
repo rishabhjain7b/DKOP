08d99d9e4ed11861567c45d7705942ce88ef3c55
rishabhjain7b <rishabhjain7b@gmail.com>
2018-05-03 15:34:53 +0530

    twelth

---
+++ System Verilog/arrays/wire_logic.sv
@@ -0,0 +1,24 @@
+// Code for wire and logic 
+
+
+module wire_logic;
+
+wire a;
+logic b;
+reg c;
+
+assign c=1;
+
+assign a=1;
+assign a=0;
+
+initial
+begin
+	b=0;
+end	
+
+initial
+begin
+	b=1;
+end
+endmodule
